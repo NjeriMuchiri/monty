15c8b6726d679ecf2817a57b18f3bae743ba71fc
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-25 23:12:10 +0300

    opcodes

--- main.c
+++ main.c
@@ -1,66 +1,26 @@
-#define _GNU_SOURCE
 #include "monty.h"
+
+char **op_toks = NULL;
+
 /**
- * error_usage - prints usage message and exits
- * Return: nothing
- */
-void error_usage(void)
-{
-fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE);
-}
-/**
- * file_error - prints file error message and exits
- * @argv: argv given by manin
- * Return: nothing
- */
-void file_error(char *argv)
-{
-fprintf(stderr, "Error: Can't open file %s\n", argv);
-exit(EXIT_FAILURE);
-}
-int status = 0;
-/**
- * main - entry point
- * @argv: list of arguments passed to our program
- * @argc: ammount of args
- * Return: nothing
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
  */
 int main(int argc, char **argv)
 {
-FILE *file;
-size_t buf_len = 0;
-char *buffer = NULL;
-char *str = NULL;
-stack_t *stack = NULL;
-unsigned int line_cnt = 1;
-global.data_struct = 1;
-if (argc != 2)
-error_usage();
-file = fopen(argv[1], "r");
-if (!file)
-file_error(argv[1]);
-while (getline(&buffer, &buf_len, file) != -1)
-{
-if (status)
-break;
-if (*buffer == '\n')
-{
-line_cnt++;
-continue;
-}
-str = strtok(buffer, " \t\n");
-if (!str || *str == '#')
-{
-line_cnt++;
-continue;
-}
-global.argument = strtok(NULL, " \t\n");
-opcode(&stack, str, line_cnt);
-line_cnt++;
-}
-free(buffer);
-free_stack(stack);
-fclose(file);
-exit(status);
+    FILE *script_fd = NULL;
+    int exit_code = EXIT_SUCCESS;
+
+    if (argc != 2)
+        return (usage_error());
+    script_fd = fopen(argv[1], "r");
+    if (script_fd == NULL)
+        return (f_open_error(argv[1]));
+    exit_code = run_monty(script_fd);
+    fclose(script_fd);
+    return (exit_code);
 }
